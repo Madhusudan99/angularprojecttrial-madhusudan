6867386df9f12531a678f420444799f6b53ea879
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-02-25 13:48:38 +0530

    Initial commit

--- my-first-app/src/app/description/description.component.ts
+++ my-first-app/src/app/description/description.component.ts
@@ -12,13 +12,66 @@ export class DescriptionComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  eventsFired = ["-1"];
+  products = [
+    {
+      "id": 1,
+      "name": "Rice",
+      "price": "12",
+      "quantity": "9",
+      "location": "Delhi",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
+    },
+    {
+      "id": 2,
+      "name": "Tomato",
+      "price": "19",
+      "quantity": "4",
+      "location": "Ahmedabad",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
+      
+    },
+    {
+      "id": 3,
+      "name": "Apple",
+      "price": "27",
+      "quantity": "49",
+      "location": "Kashmir",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
 
-  @Output() descriptionSectionEventEmiter = new EventEmitter<string>();
-  addEvent(newEvent:string) {
-    this.eventsFired.push(newEvent);
-    this.descriptionSectionEventEmiter.emit("add2Cart");
+    },
+    {
+      "id": 4,
+      "name": "Jam",
+      "price": "56",
+      "quantity": "79",
+      "location": "Delhi",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
 
+    },
+    {
+      "id": 5,
+      "name": "Honey",
+      "price": "12",
+      "quantity": "9",
+      "location": "Delhi",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
+
+    },
+    {
+      "id": 6,
+      "name": "Tea",
+      "price": "12",
+      "quantity": "9",
+      "location": "Delhi",
+      "url": "https://www.bigbasket.com/media/uploads/p/s/40075897_12-bb-royal-sona-masoori-rice-raw-rice-super-premium.jpg",
+
+    },
+  ];
+
+  @Output() descriptionItemEvent = new EventEmitter<any>();
+
+  addToCartEvent(productData: any) {
+    this.descriptionItemEvent.emit(productData);
   }
 
 }
